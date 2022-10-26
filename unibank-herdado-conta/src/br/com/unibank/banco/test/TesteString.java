71d795c97fcf4077e9e500ed401b025a3f9a5878
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-10-26 09:42:26 -0300

    Criando teste de String e sua imutabilidade

---
+++ unibank-herdado-conta/src/br/com/unibank/banco/test/TesteString.java
@@ -0,0 +1,19 @@
+package br.com.unibank.banco.test;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		
+		String nome = "Alura";//object literal
+		//String outro = new String("Alura");
+		
+		//String outra = nome.replace("A", "a");
+		
+		String outra = nome.toLowerCase();
+		
+		System.out.println(nome);
+		System.out.println(outra);
+		
+	}
+
+}
