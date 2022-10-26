d8b6b0315c7fa3ecf8a6237c9f69706cf986efab
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-10-26 16:48:22 -0300

    Criando o metodo toString nas contas

--- unibank-herdado-conta/src/br/com/unibank/banco/test/Teste.java
+++ unibank-herdado-conta/src/br/com/unibank/banco/test/Teste.java
@@ -9,16 +9,16 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		System.out.println("x");
-		System.out.println(3);
-		System.out.println(false);
+//		System.out.println("x");
+//		System.out.println(3);
+//		System.out.println(false);
 		
-		ContaCorrente cc = new ContaCorrente(22, 33);
-		Object cp = new ContaPoupanca(33, 22);	
+		Object cc = new ContaCorrente(123, 333);
+		Object cp = new ContaPoupanca(321, 222);	
 		Object cliente = new Cliente();
 		
 		
-		System.out.println(cc.toString());
+		System.out.println(cc);
 		System.out.println(cp);
 		
 //		println(cliente);
