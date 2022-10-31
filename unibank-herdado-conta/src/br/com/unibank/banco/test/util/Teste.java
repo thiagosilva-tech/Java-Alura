c1fdaf06dc8f83a8de3760c19b6a06cf14348036
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-10-31 09:50:12 -0300

    Criando teste para o metodo Integer

--- unibank-herdado-conta/src/br/com/unibank/banco/test/util/Teste.java
+++ unibank-herdado-conta/src/br/com/unibank/banco/test/util/Teste.java
@@ -6,13 +6,24 @@ import java.util.List;
 public class Teste {
 	
 	public static void main(String[] args) {
+				
+		int idade = 29;
 		
-		int[] idades = new int[5];
+		Integer idadeRef = Integer.valueOf(29);//autoboxing
+		System.out.println(idadeRef.doubleValue());
 		
-		String[] nomes = new String[5];
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
 		
-		int idade = 29;//Integer
-		Integer idadeRef = new Integer(29);
+		System.out.println(Integer.SIZE + " bits");
+		System.out.println(Integer.BYTES + " bytes");
+		
+		int valor = idadeRef.intValue();//unboxing		
+		String s = args[0];//"10"		
+		//Integer numero = Integer.valueOf(s);
+		int numero = Integer.parseInt(s);
+		System.out.println(numero);
+		
 		List<Integer> numeros = new ArrayList<Integer>();
 		numeros.add(29); //Autoboxing
 		
