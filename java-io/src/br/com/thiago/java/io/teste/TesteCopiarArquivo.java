ca529d645856bd72dc3d0de91f367557adf1166e
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-11-02 16:21:48 -0300

    Criando teste para entradas e saidas de dados

---
+++ java-io/src/br/com/thiago/java/io/teste/TesteCopiarArquivo.java
@@ -0,0 +1,41 @@
+package br.com.thiago.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.Socket;
+
+public class TesteCopiarArquivo {
+	public static void main(String[] args) throws IOException {
+		
+		Socket s = new Socket();
+		
+		InputStream fis = s.getInputStream();//Rede //System.in;Console //new FileInputStream("poema.txt");Teclado
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		OutputStream fos = s.getOutputStream();//Rede //System.out;Console //new FileOutputStream("poema2.txt");Teclado
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		String linha = br.readLine();
+		
+		while(linha != null && !linha.isEmpty()) {
+			bw.write(linha);
+			bw.newLine();
+			bw.flush();
+			linha = br.readLine();
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
