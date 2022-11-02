9d9829a4c7e5bab4e26ee39f532af9db6ec17462
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-11-02 16:54:59 -0300

    Criando teste de PrintWriter e PrintStream

--- java-io/src/br/com/thiago/java/io/teste/TesteLeitura.java
+++ java-io/src/br/com/thiago/java/io/teste/TesteLeitura.java
@@ -2,7 +2,6 @@ package br.com.thiago.java.io.teste;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
