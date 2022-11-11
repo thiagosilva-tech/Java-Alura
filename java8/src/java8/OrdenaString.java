8eac1ffe005209514d56283dcd9a3dd7face8285
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-11-11 09:46:23 -0300

    Conhecendo sobre lambdas

--- java8/src/java8/OrdenaString.java
+++ java8/src/java8/OrdenaString.java
@@ -1,7 +1,6 @@
 package java8;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -12,39 +11,26 @@ public class OrdenaString {
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
 
-		Comparator<String> comparador = new ComparadorPorTamanho();
-// 		Collections.sort(palavras, comparador);
-		palavras.sort(comparador);
+		
+//		palavras.sort((s1, s2) -> {
+//				if (s1.length() < s2.length())
+//					return -1;
+//				if (s1.length() > s2.length())
+//					return 1;
+//				return 0;
+//			});
+		
+//		palavras.sort((s1, s2) -> s1.length() - s2.length());
+		
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+
+		
 		System.out.println(palavras);
-
-//		for (String p : palavras) {
-//			System.out.println(p);
-//		}
-
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+		
+		Consumer<String> impressor = s -> System.out.println(s);
+		palavras.forEach(impressor);
+		
+		palavras.forEach(s -> System.out.println(s));
 	}
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 1;
-		return 0;
-	}
-
 }
