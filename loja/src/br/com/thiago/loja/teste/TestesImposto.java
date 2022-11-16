a75b3528f8e736b44ee39d3fd83f12e0bb750ce9
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-11-16 17:07:09 -0300

    Conhecendo o Patterns Decorator

---
+++ loja/src/br/com/thiago/loja/teste/TestesImposto.java
@@ -0,0 +1,19 @@
+package br.com.thiago.loja.teste;
+
+import java.math.BigDecimal;
+
+import br.com.thiago.loja.imposto.CalculadoraDeImpostos;
+import br.com.thiago.loja.imposto.ICMS;
+import br.com.thiago.loja.imposto.ISS;
+import br.com.thiago.loja.orcamento.Orcamento;
+
+public class TestesImposto {
+
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		
+		System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
+	}
+
+}
