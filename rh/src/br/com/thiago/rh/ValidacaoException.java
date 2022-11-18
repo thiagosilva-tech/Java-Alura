8944ba4bb572f85536efe9f410c7423c10e75d87
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-11-18 09:40:47 -0300

    Melhorando a coesão do codigo

---
+++ rh/src/br/com/thiago/rh/ValidacaoException.java
@@ -0,0 +1,11 @@
+package br.com.thiago.rh;
+
+public class ValidacaoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ValidacaoException(String mensagem) {
+		super(mensagem);
+	}
+
+}
---
+++ rh/src/br/com/thiago/rh/service/ReajusteService.java
@@ -0,0 +1,20 @@
+package br.com.thiago.rh.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.thiago.rh.ValidacaoException;
+import br.com.thiago.rh.model.Funcionario;
+
+public class ReajusteService {
+	
+	public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+		BigDecimal salarioAtual = funcionario.getSalario();
+		BigDecimal percentualReajuste = aumento.divide(salarioAtual , RoundingMode.HALF_UP);
+		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+		}
+		BigDecimal salarioReajustado = salarioAtual.add(aumento);
+		funcionario.atualizarSalario(salarioReajustado);
+	}
+}
