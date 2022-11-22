a20ffa7e37ed02c8a082035748e60f9bf7cb467f
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-11-22 11:44:20 -0300

    Criando teste automatizados com Junit

---
+++ junit/src/br/com/thiago/tdd/CalculadoraTest.java
@@ -0,0 +1,17 @@
+package br.com.thiago.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+public class CalculadoraTest {
+	
+	@Test
+	public void deveriaSomarDoisNumerosPositivos() {
+		Calculadora calc = new Calculadora();
+		int soma = calc.somar(3, 7);
+		
+		Assert.assertEquals(10, soma);
+	}
+	
+}
