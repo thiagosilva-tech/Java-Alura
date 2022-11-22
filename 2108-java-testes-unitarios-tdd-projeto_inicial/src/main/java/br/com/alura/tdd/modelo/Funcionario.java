aba1e43dced2c46d3f4f8b6eb95d88ae3003e729
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-11-22 17:00:16 -0300

    Testando metodos privados

--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Funcionario.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -29,8 +29,13 @@ public class Funcionario {
 	}
 
 	public void rejustarSalario(BigDecimal reajuste) {
-		this.salario = this.salario.add(reajuste).setScale(2, RoundingMode.HALF_UP);		
+		this.salario = this.salario.add(reajuste);	
+		arredondarSalario();
 		
+	}
+
+	private void arredondarSalario() {
+		this.salario = this.salario.setScale(2, RoundingMode.HALF_UP);
 	}
 
 }
