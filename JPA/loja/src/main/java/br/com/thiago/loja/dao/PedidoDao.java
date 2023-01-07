3de7d03274e48d17a0c8d1b9e39c8dd6cc71df24
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-07 11:30:15 -0300

    Consultas para relatorios

--- JPA/loja/src/main/java/br/com/thiago/loja/dao/PedidoDao.java
+++ JPA/loja/src/main/java/br/com/thiago/loja/dao/PedidoDao.java
@@ -1,6 +1,7 @@
 package br.com.thiago.loja.dao;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -23,7 +24,17 @@ public class PedidoDao {
 		return em.createQuery(jpql, BigDecimal.class).getSingleResult();
 	}
 	
-	
+	public List<Object[]> relatorioDeVendas(){
+		String jpql = "SELECT produto.nome, "
+				+ "SUM(item.quantidade),"
+				+ "MAX(pedido.data) "
+				+ "FROM Pedido pedido "
+				+ "JOIN pedido.itens item "
+				+ "JOIN item.produto produto "
+				+ "GROUP BY produto.nome "
+				+ "ORDER BY item.quantidade DESC";
+		return em.createQuery(jpql, Object[].class).getResultList();
+	}
 	
 	
 	
--- JPA/loja/src/main/java/br/com/thiago/loja/testes/CadastroDePedido.java
+++ JPA/loja/src/main/java/br/com/thiago/loja/testes/CadastroDePedido.java
@@ -1,6 +1,7 @@
 package br.com.thiago.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -22,22 +23,30 @@ public class CadastroDePedido {
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		ClienteDao clienteDao = new ClienteDao(em);
-		
-		Produto produto = produtoDao.buscarPorId(1l);	
+
+		Produto produto = produtoDao.buscarPorId(1l);
 		Cliente cliente = clienteDao.buscarPorId(1l);
-		
+
 		em.getTransaction().begin();
-		
+
 		Pedido pedido = new Pedido(cliente);
 		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
-		
+
 		PedidoDao pedidoDao = new PedidoDao(em);
-		pedidoDao.cadastrar(pedido);	
-				
+		pedidoDao.cadastrar(pedido);
+
 		em.getTransaction().commit();
-		
+
 		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
 		System.out.println("Valor total: " + totalVendido);
+
+		List<Object[]> relatorio = pedidoDao.relatorioDeVendas();
+		for (Object[] obj : relatorio) {
+			System.out.println(obj[0]);
+			System.out.println(obj[1]);
+			System.out.println(obj[2]);
+		}
+
 	}
 
 	private static void popularBancoDeDados() {
@@ -45,7 +54,7 @@ public class CadastroDePedido {
 		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 
 		Cliente cliente = new Cliente("Rodrigo", "123456");
-		
+
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		CategoriaDao categoriaDao = new CategoriaDao(em);
