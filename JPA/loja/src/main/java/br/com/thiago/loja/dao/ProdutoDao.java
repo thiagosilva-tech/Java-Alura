1094d0d4cfd1c6ffd60c066d88ed790a2fda14e1
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-09 11:22:14 -0300

    Consultas com parametros dinamicos

--- JPA/loja/src/main/java/br/com/thiago/loja/dao/ProdutoDao.java
+++ JPA/loja/src/main/java/br/com/thiago/loja/dao/ProdutoDao.java
@@ -1,9 +1,11 @@
 package br.com.thiago.loja.dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import br.com.thiago.loja.modelo.Produto;
 
@@ -14,52 +16,67 @@ public class ProdutoDao {
 	public ProdutoDao(EntityManager em) {
 		this.em = em;
 	}
-	
+
 	public void cadastrar(Produto produto) {
 		this.em.persist(produto);
 	}
-	
+
 	public void atualizar(Produto produto) {
 		this.em.merge(produto);
 	}
-	
+
 	public void remover(Produto produto) {
 		produto = em.merge(produto);
 		this.em.remove(produto);
 	}
-	
+
 	public Produto buscarPorId(Long id) {
 		return em.find(Produto.class, id);
 	}
-	
-	public List<Produto> buscarTodos(){
+
+	public List<Produto> buscarTodos() {
 		String jpql = "SELECT p FROM Produto p";
 		return em.createQuery(jpql, Produto.class).getResultList();
 	}
-	
-	public List<Produto> buscarPorNome(String nome){
+
+	public List<Produto> buscarPorNome(String nome) {
 		String jpql = "SELECT p FROM Produto p WHERE p.nome = ?1";
-		return em.createQuery(jpql, Produto.class)
-				.setParameter(1, nome)
-				.getResultList();
+		return em.createQuery(jpql, Produto.class).setParameter(1, nome).getResultList();
 	}
-	
-	public List<Produto> buscarPorNomeDaCategoria(String nome){
-		return em.createNamedQuery("Produto.produtosPorCategoria", Produto.class)
-				.setParameter(1, nome)
-				.getResultList();
+
+	public List<Produto> buscarPorNomeDaCategoria(String nome) {
+		return em.createNamedQuery("Produto.produtosPorCategoria", Produto.class).setParameter(1, nome).getResultList();
 	}
-	
-	public BigDecimal buscarPrecoDoProdutoComNome(String nome){
+
+	public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
 		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = ?1";
-		return em.createQuery(jpql, BigDecimal.class)
-				.setParameter(1, nome)
-				.getSingleResult();
+		return em.createQuery(jpql, BigDecimal.class).setParameter(1, nome).getSingleResult();
 	}
-	
-	
-	
-	
-	
-	
+
+	public List<Produto> buscarPorParamentros(String nome, BigDecimal preco, LocalDate dataCadastro) {
+		String jpql = "SELECT p FROM Produto p WHERE 1=1 ";
+		if (nome != null && !nome.trim().isEmpty()) {
+			jpql = " AND p.nome = :nome ";
+		}
+		if (preco != null) {
+			jpql = " AND p.preco = :preco ";
+		}
+		if (dataCadastro != null) {
+			jpql = " AND p.dataCadastro = :dataCadastro ";
+		}
+
+		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+
+		if (nome != null && !nome.trim().isEmpty()) {
+			query.setParameter("nome", nome);
+		}
+		if (preco != null) {
+			query.setParameter("preco", preco);
+		}
+		if (dataCadastro != null) {
+			query.setParameter("dataCadastro", dataCadastro);
+		}
+		return query.getResultList();
+	}
+
 }
