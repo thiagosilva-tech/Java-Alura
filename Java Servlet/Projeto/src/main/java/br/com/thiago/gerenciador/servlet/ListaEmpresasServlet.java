46c793fb631d43a4220e794cd040a5ce3dfab24c
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-13 16:36:25 -0300

    JSP para listar empresas

--- Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/servlet/ListaEmpresasServlet.java
+++ Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/servlet/ListaEmpresasServlet.java
@@ -1,9 +1,9 @@
 package br.com.thiago.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,22 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/listaEmpresas")
 public class ListaEmpresasServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
-		PrintWriter out = response.getWriter();
-
-		out.println("<html><body>");
-		out.println("<ul>");
-
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-
-		out.println("</ul>");
-		out.println("</body></html>");
+		
+		request.setAttribute("empresas", lista);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);		
 	}
-
 }
