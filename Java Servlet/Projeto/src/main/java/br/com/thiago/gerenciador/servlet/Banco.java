e43592cdd78f13bd3b77338897c4afe0faa5754c
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-16 10:22:52 -0300

    Removendo empresa

--- Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/servlet/Banco.java
+++ Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/servlet/Banco.java
@@ -1,29 +1,45 @@
 package br.com.thiago.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> listaEmpresas = new ArrayList<>();
-	
+	private static Integer chaveSequencial = 1;
+
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
-		
 		listaEmpresas.add(empresa);
 		listaEmpresas.add(empresa2);
-		
+
 	}
-	
+
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		listaEmpresas.add(empresa);
-	}	
-	
-	public List<Empresa> getEmpresas(){
+
+	}
+
+	public List<Empresa> getEmpresas() {
 		return Banco.listaEmpresas;
 	}
-	
+
+	public void removeEmpresa(Integer id) {
+
+		Iterator<Empresa> it = listaEmpresas.iterator();
+
+		while (it.hasNext()) {
+			Empresa emp = it.next();
+			if (emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
 }
