52eed5482e6735bdac39d7ae4c455fa5efd7db0a
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-20 10:53:33 -0300

    Preparando modelo e view

--- Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/modelo/Banco.java
+++ Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> listaEmpresas = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 
 	static {
@@ -18,7 +19,17 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		listaEmpresas.add(empresa);
 		listaEmpresas.add(empresa2);
-
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("Thiago");
+		u1.setSenha("12345");
+		Usuario u2 = new Usuario();
+		u2.setLogin("Juh");
+		u2.setSenha("12345");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
+		
 	}
 
 	public void adiciona(Empresa empresa) {
