e61e876c43d8a482dd1dd3695f825efbd4af7195
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-20 11:03:30 -0300

    Verificando o login

--- Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/acao/Login.java
+++ Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/acao/Login.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.thiago.gerenciador.modelo.Banco;
+import br.com.thiago.gerenciador.modelo.Usuario;
+
 public class Login implements Acao {
 
 	@Override
@@ -17,7 +20,14 @@ public class Login implements Acao {
 		
 		System.out.println("Logando " + login);
 		
+		Banco banco = new Banco();
+		Usuario usuario = banco.existeUsuario(login, senha);
 		
-		return "redirect:entrada?acao=ListaEmpresas";
+		if(usuario != null) {
+			System.out.println("Usuario existe");
+			return "redirect:entrada?acao=ListaEmpresas";
+		} else {
+			return "redirect:entrada?acao=LoginForm";
+		}		
 	}
 }
