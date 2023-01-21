1ebf294be35981d169dcdc5c4bc207631ec9aa36
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-21 11:19:25 -0300

    Configurando ordem de execução dos filtros

--- Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/servlet/AutorizacaoFilter.java
+++ Java Servlet/Projeto/src/main/java/br/com/thiago/gerenciador/servlet/AutorizacaoFilter.java
@@ -7,13 +7,12 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/entrada")
+//@WebFilter("/entrada")
 public class AutorizacaoFilter extends HttpFilter implements Filter {
 
 	private static final long serialVersionUID = 1L;
