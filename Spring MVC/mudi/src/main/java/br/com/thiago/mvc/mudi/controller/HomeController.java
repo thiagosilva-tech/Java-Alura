624d8799b76da751947029d4d25a0b46820a0374
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-25 10:00:43 -0300

    Usando JPA puro

--- Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/controller/HomeController.java
+++ Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,10 @@
 package br.com.thiago.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +15,17 @@ import br.com.thiago.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
-	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 8");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/41YvfoWY9KL._AC_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07ZJZ7VW6/ref=sr_1_5?__mk_pt_BR=ÅMÅŽÕÑ&crid=EKETUV2ZJWFP&keywords=Xiaomi+Redmi+Note+8&qid=1674583717&sprefix=xiaomi+redmi+note+8%2Caps%2C173&sr=8-5&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
-		pedido.setDescricao("Uma descrição qualquer para esse pedido");
+	public String home(Model model) {		
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
-		model.addAttribute("pedidos", pedidos);
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		
+		List<Pedido> pedidos = query.getResultList();
+		
+		model.addAttribute("pedidos", pedidos);		
 		return "home";
 	}
 }
