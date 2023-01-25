6273077c78d57a535b61395c649b3894e1e6e0df
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-25 09:17:11 -0300

    Aplicando o Bootstrap

--- Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/controller/HomeController.java
+++ Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,27 @@
 package br.com.thiago.mvc.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.thiago.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xiaomi Redmi Note 8");
+		pedido.setUrlImagem("https://m.media-amazon.com/images/I/41YvfoWY9KL._AC_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07ZJZ7VW6/ref=sr_1_5?__mk_pt_BR=ÅMÅŽÕÑ&crid=EKETUV2ZJWFP&keywords=Xiaomi+Redmi+Note+8&qid=1674583717&sprefix=xiaomi+redmi+note+8%2Caps%2C173&sr=8-5&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
+		pedido.setDescricao("Uma descrição qualquer para esse pedido");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
 }
