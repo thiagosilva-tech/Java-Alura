346b7474afe2274beb32a0fcd093c39fd1330e7b
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-27 10:04:04 -0300

    Validação do pedido

--- Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -1,38 +1,54 @@
 package br.com.thiago.mvc.mudi.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import br.com.thiago.mvc.mudi.model.Pedido;
 
 public class RequisicaoNovoPedido {
 
+	@NotBlank
 	private String nomeProduto;
+
+	@NotBlank
 	private String urlProduto;
+
+	@NotBlank
 	private String urlImagem;
+
 	private String descricao;
-	
+
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
+
 	public void setNomeProduto(String nomeProduto) {
 		this.nomeProduto = nomeProduto;
 	}
+
 	public String getUrlProduto() {
 		return urlProduto;
 	}
+
 	public void setUrlProduto(String urlProduto) {
 		this.urlProduto = urlProduto;
 	}
+
 	public String getUrlImagem() {
 		return urlImagem;
 	}
+
 	public void setUrlImagem(String urlImagem) {
 		this.urlImagem = urlImagem;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public Pedido toPedido() {
 		Pedido pedido = new Pedido();
 		pedido.setDescricao(descricao);
@@ -41,5 +57,5 @@ public class RequisicaoNovoPedido {
 		pedido.setUrlProduto(urlProduto);
 		return pedido;
 	}
-	
+
 }
