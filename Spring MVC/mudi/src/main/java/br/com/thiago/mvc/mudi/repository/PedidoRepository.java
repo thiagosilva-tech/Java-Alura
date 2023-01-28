06f173bb30000a47521bf867e34ece47d586e0fd
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-28 10:10:45 -0300

    Configurando as rotas de status

--- Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/repository/PedidoRepository.java
+++ Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/repository/PedidoRepository.java
@@ -1,10 +1,15 @@
 package br.com.thiago.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.thiago.mvc.mudi.model.Pedido;
+import br.com.thiago.mvc.mudi.model.StatusPedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{	
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+	List<Pedido> findByStatus(StatusPedido status);	
 }
