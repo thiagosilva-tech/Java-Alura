c63d12fb397fda58635a61c2f75789f1587ac3cd
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-01-30 12:12:42 -0300

    Mostrando os pedidos do usuario

--- Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/repository/PedidoRepository.java
+++ Spring MVC/mudi/src/main/java/br/com/thiago/mvc/mudi/repository/PedidoRepository.java
@@ -3,6 +3,8 @@ package br.com.thiago.mvc.mudi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.thiago.mvc.mudi.model.Pedido;
@@ -11,5 +13,8 @@ import br.com.thiago.mvc.mudi.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
-	List<Pedido> findByStatus(StatusPedido status);	
+	List<Pedido> findByStatus(StatusPedido status);
+
+	@Query("select p from Pedido p join p.user u where u.username = :username")
+	List<Pedido> findAllByUsuario(@Param("username")String username);	
 }
