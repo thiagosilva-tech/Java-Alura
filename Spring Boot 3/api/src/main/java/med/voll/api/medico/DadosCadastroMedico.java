88b26748288d54d36acf050f917108e5cf49a513
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-02-07 09:43:28 -0300

    Nova migration

--- Spring Boot 3/api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ Spring Boot 3/api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -10,12 +10,18 @@ import med.voll.api.endereco.DadosEndereco;
 public record DadosCadastroMedico(
         @NotBlank
         String nome,
+
         @NotBlank
         @Email
         String email,
+
+        @NotBlank
+        String telefone,
+
         @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
+
         @NotNull
         Especialidade especialidade,
         @NotNull @Valid DadosEndereco endereco) {
