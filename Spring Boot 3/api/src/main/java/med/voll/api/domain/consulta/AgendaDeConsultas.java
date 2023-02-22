a4dafde2aa51597ae029c3fcfa729dd2b9129b05
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-02-22 10:40:16 -0300

    Escolha de medico aleatorio por uma Query no Repository

--- Spring Boot 3/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ Spring Boot 3/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -29,13 +29,22 @@ public class AgendaDeConsultas {
         }
 
         var medico = escolherMedico(dados);
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
-        
+        if(dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido!")
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+
     }
 
 }
